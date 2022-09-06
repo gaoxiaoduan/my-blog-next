6f87461a19c60a14fdee848b21253e0aec31f838
coderduan <1961018000@qq.com>
2022-09-06 17:30:25 +0800

    🐳 chore: 添加mockjs

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,17 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { mock } from 'mockjs';
 
 type Data = {
-  name: string
-}
+  name: string,
+};
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  res.status(200).json(
+    mock({
+      'list|1-10': [{ 'id|+1': 1 }],
+    })
+  );
 }
